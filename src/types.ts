779b2693b80164b051de65688df82a6e65f84ab0
PollyMesser <145049806+PollyMesser@users.noreply.github.com>
2024-12-09 09:51:46 +0100

    initial commit

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface Event {
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  description: string;
+  learnMoreUrl: string;
+  registerUrl: string;
+}
+
+export interface NewsletterData {
+  title: string;
+  date: string;
+  greeting: string;
+  introduction: string;
+  events: Event[];
+  closingMessage: string;
+  senderName: string;
+}
